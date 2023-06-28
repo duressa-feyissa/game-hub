6e774326e619cd388c6029d1a92b79ca1e44dcd4
duressa-feyissa <duresafeyisa2022@gmail.com>
2023-06-28 11:03:18 +0300

    Add Trailers

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -3,6 +3,7 @@ import { GameAttributes } from "../components/GameAttributes";
 import { Spinner, Heading } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
+import GameTrailer from "../components/GameTrailer";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -17,6 +18,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText children={game.description_raw} />
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
     </>
   );
 };
